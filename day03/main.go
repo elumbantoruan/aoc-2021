b3e4b96b0903849a606e7cf24549a80f87fac498
Edison Lumbantoruan <edison.lumbantoruan@wunderkind.co>
2021-12-09 19:12:19 -0800

    add solution for day3b

--- day03/main.go
+++ day03/main.go
@@ -17,6 +17,10 @@ func main() {
 	lines := strings.Split(text, "\n")
 	power := powerConsumption(lines)
 	fmt.Println(power)
+
+	val := powerConsumption2(lines)
+	fmt.Println(val)
+
 }
 
 func powerConsumption(lines []string) int64 {
@@ -52,4 +56,64 @@ func powerConsumption(lines []string) int64 {
 	epsilonDec, _ := strconv.ParseInt(string(epsilon), 2, 64)
 
 	return gammaDec * epsilonDec
+}
+
+func powerConsumption2(lines []string) int64 {
+	var (
+		bits         = lines[0]
+		oxygenSource = lines
+		co2Source    = lines
+	)
+	for i := 0; i < len(bits); i++ {
+		zeroBitCount, oneBitCount := mostCommonValue(oxygenSource, i)
+		if zeroBitCount > oneBitCount {
+			oxygenSource = subList(oxygenSource, i, '0')
+		} else {
+			oxygenSource = subList(oxygenSource, i, '1')
+		}
+		if len(oxygenSource) == 1 {
+			break
+		}
+	}
+
+	for i := 0; i < len(bits); i++ {
+		zeroBitCount, oneBitCount := mostCommonValue(co2Source, i)
+		if zeroBitCount < oneBitCount || zeroBitCount == oneBitCount {
+			co2Source = subList(co2Source, i, '0')
+		} else {
+			co2Source = subList(co2Source, i, '1')
+		}
+		if len(co2Source) == 1 {
+			break
+		}
+	}
+
+	oxygen := oxygenSource[0]
+	co2 := co2Source[0]
+
+	oxygenVal, _ := strconv.ParseInt(oxygen, 2, 64)
+	co2Val, _ := strconv.ParseInt(co2, 2, 64)
+
+	return oxygenVal * co2Val
+}
+
+func mostCommonValue(lines []string, pos int) (zeroBitCount int, oneBitCount int) {
+	for _, line := range lines {
+		if line[pos] == '0' {
+			zeroBitCount++
+		} else {
+			oneBitCount++
+		}
+	}
+	return
+}
+
+func subList(lines []string, pos int, targetVal byte) []string {
+	var list []string
+	for _, line := range lines {
+		if line[pos] == targetVal {
+			list = append(list, line)
+		}
+	}
+	return list
 }
